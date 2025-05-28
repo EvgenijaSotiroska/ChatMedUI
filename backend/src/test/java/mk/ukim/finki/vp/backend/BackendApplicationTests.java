80661daef6ffb7743899adff0cf7764d61ce66c8
angelaangeleska <angela.angeleska@students.finki.ukim.mk>
2025-05-28 14:25:30 +0200

    initial commit

---
+++ backend/src/test/java/mk/ukim/finki/vp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.vp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
